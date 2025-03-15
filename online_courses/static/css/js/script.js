d3e338273e9f5fe4c9bfe2e404cd9210cdb91d11
LilitAsa <lilitasa24@yahoo.com>
2025-03-15 17:43:16 +0300

    Add theme toggle functionality and update styles for dark mode support

---
+++ online_courses/static/css/js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-theme");
+    document.getElementById("theme-icon").classList.replace("fa-moon", "fa-sun");
+  }
+});
+function toggleTheme() {
+  document.body.classList.toggle("dark-theme");
+  const icon = document.getElementById("theme-icon");
+  if (document.body.classList.contains("dark-theme")) {
+    localStorage.setItem("theme", "dark");
+    icon.classList.replace("fa-moon", "fa-sun");
+  } else {
+    localStorage.setItem("theme", "light");
+    icon.classList.replace("fa-sun", "fa-moon");
+  }
+}
